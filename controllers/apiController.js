e6c289c658f48c2086a519d4f08ef0107bb5fe74
Bagas Ramadhani <56336128+bagasrmdhn@users.noreply.github.com>
2023-02-12 17:59:05 +0700

    tes 2

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -8,7 +8,7 @@ module.exports = {
   landingPage: async (req, res) => {
     try {
       const item = await Item.find()
-        .select("_id name  imageId categoryId")
+        .select("_id imageId categoryId")
         .populate({ path: "categoryId", select: "_id " })
         .populate({
           path: "imageId",
