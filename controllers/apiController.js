ae5cf1bb1e5717475531534a7f5ca7d1a85bc6fa
Bagas Ramadhani <56336128+bagasrmdhn@users.noreply.github.com>
2023-02-12 17:54:24 +0700

    categoryId on landing page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,11 +7,14 @@ const Order = require("../models/Order");
 module.exports = {
   landingPage: async (req, res) => {
     try {
-      const item = await Item.find().select("_id name price imageId").populate({
-        path: "imageId",
-        limit: 1,
-        select: "_id imageUrl",
-      });
+      const item = await Item.find()
+        .select("_id name price imageId categoryId")
+        .populate({ path: "categoryId", select: "_id name" })
+        .populate({
+          path: "imageId",
+          limit: 1,
+          select: "_id imageUrl",
+        });
       const category = await Category.find().select("_id name");
 
       res.status(200).json({ category, item });
