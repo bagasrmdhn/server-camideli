95aa7c95d91eb855b0a975d4be284a39fabade6c
Bagas Ramadhani <bagasrmdhn14@gmail.com>
2023-02-15 15:22:30 +0700

    tes

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -70,12 +70,12 @@ module.exports = {
         accountHolder === undefined ||
         bankFrom === undefined
       ) {
-        return res.status(404).json({ message: error.message });
+        return res.status(404).json({ message: "Please complete all field" });
       }
 
       const item = await Item.findOne({ _id: idItem });
       if (!item) {
-        return res.status(404).json({ message: error.message });
+        return res.status(404).json({ message: "Item not found" });
       }
 
       item.sumOrder += 1;
