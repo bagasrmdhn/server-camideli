7e468b656828a3c17b7e7c0cbc08938b5fb61480
Bagas Ramadhani <bagasrmdhn14@gmail.com>
2023-02-17 20:40:25 +0700

    undo auth verif

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const apiController = require("../controllers/apiController");
 const { uploadSingle } = require("../middleware/multer");
-const { verifyUser } = require("../middleware/authUser");
 
 router.get("/landing-page", apiController.landingPage);
 router.post("/sign-up", uploadSingle, apiController.signUp);
@@ -9,6 +8,6 @@ router.post("/sign-in", uploadSingle, apiController.signIn);
 router.get("/get-me", uploadSingle, apiController.getMe);
 router.delete("/logout", uploadSingle, apiController.logOut);
 router.get("/detail-page/:id", apiController.detailPage);
-router.post("/order-page", verifyUser, uploadSingle, apiController.orderPage);
+router.post("/order-page", uploadSingle, apiController.orderPage);
 
 module.exports = router;
